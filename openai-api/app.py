cf63ee061c6338e0c24a83f758d1b9cc2b6156e1
Jacob Madsen <jmmadsen16@gmail.com>
2023-12-10 17:12:11 -0600

    routes for testing async calls from chatbot

--- openai-api/app.py
+++ openai-api/app.py
@@ -6,4 +6,12 @@ app = Flask(__name__)
 # ping health of app
 @app.route("/health_check")
 def health_check():
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'} 
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+@app.route("/docs_chain_query")
+def docs_chain_query():
+    return "docs chain"
+
+@app.route("/db_chain_query")
+def db_chain_query():
+    return "db chain"
