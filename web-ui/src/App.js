2226908ee9fc0c2cb1d056a23f08726bab47601a
Jacob Madsen <jmmadsen16@gmail.com>
2023-12-10 16:47:29 -0600

    chatbot foundations

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+
+
+const steps = [
+  {
+    id: '0',
+    message: 'Hey there! I am your 2023 MLB season AI assistant.',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    message: 'Would you like to query the batting database, or ask a general question about the season?',
+    trigger: '2',
+  },
+  {
+    id: '2',
+    options: [
+      { value: 'Query the database', label: 'Query the database', trigger: 'database' },
+      { value: 'General question', label: 'General question', trigger: 'docs' }
+    ],
+  },
+  {
+    id: 'database',
+    message: `Please enter your question about the season's batting stats:`,
+    trigger: 'database-q',
+  },
+  {
+    id: 'database-q',
+    user: true,
+    trigger: 'database-res',
+  },
+  {
+    id: 'database-res',
+    message: 'restart!',
+    trigger: '1',
+  },
+  {
+    id: 'docs',
+    message: `Please enter your question about this season:`,
+    trigger: 'docs-q',
+  },
+  {
+    id: 'docs-q',
+    user: true,
+    trigger: 'docs-res',
+  },
+  {
+    id: 'docs-res',
+    message: 'restart!',
+    trigger: '1',
+  }
+];
+
+const theme = {
+  background: '#C9FF8F',
+  headerBgColor: '#197B22',
+  headerFontSize: '20px',
+  botBubbleColor: '#0F3789',
+  headerFontColor: 'white',
+  botFontColor: 'white',
+  userBubbleColor: '#FF5733',
+  userFontColor: 'white',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="App-header">
+      <ThemeProvider theme={theme}>
+          <h1>ChatGPT MLB Assistant</h1>
+          <ChatBot steps={steps} />
+      </ThemeProvider>
+    </header>
+);
 }
 
 export default App;
