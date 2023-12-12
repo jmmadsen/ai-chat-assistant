fd22d9ca7d3eaa48e97f8d5c402e6e08e48a8ccf
Jacob Madsen <jmmadsen16@gmail.com>
2023-12-11 18:56:02 -0600

    added exception handling to api

--- openai-api/app.py
+++ openai-api/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 import json
 from flask_cors import CORS
 
+# utility functions
+from utils.exception_handler import exception_handler
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -11,10 +14,20 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def health_check():
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
+# ask qualitative questions against training documents
 @app.route("/docs_chain_query")
 def docs_chain_query():
-    return "docs chain"
+    try:
+        return "docs chain"
+    except Exception as err:
+        error = exception_handler(err)
+        return error
 
+# ask quantitative questions against Postgresql - NLP to SQL to NLP
 @app.route("/db_chain_query")
 def db_chain_query():
-    return "db chain"
+    try:
+        return "db chain"
+    except Exception as err:
+        error = exception_handler(err)
+        return error
---
+++ openai-api/utils/exception_handler.py
@@ -0,0 +1,5 @@
+import logging
+
+def exception_handler(err):
+  logging.error(err)
+  return 'Sorry, there was an internal error on my end! Please try chatting again later.'
